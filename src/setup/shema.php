2801cf3aa0c33577d3b6db5cf4e2dbdb2121d26d
Alexander Kuzmin <roosit@abricos.org>
2016-04-04 12:45:34 +0300

    workfx

--- src/setup/shema.php
+++ src/setup/shema.php
@@ -12,14 +12,7 @@ $updateManager = Ab_UpdateManager::$current;
 $db = Abricos::$db;
 $pfx = $db->prefix;
 
-$uprofileManager = Abricos::GetModule('uprofile')->GetManager();
-
 if ($updateManager->isInstall()){
-
-    $uprofileManager->FieldAppend('lastname', 'Фамилия', UserFieldType::STRING, 100);
-    $uprofileManager->FieldAppend('firstname', 'Имя', UserFieldType::STRING, 100);
-    $uprofileManager->FieldCacheClear();
-
     Abricos::GetModule('support')->permission->Install();
 
     // проекты
